a721dda6fe17dc90b4cdc312479639060cbaa08f
faikguler <faikguler@faikguler.com>
2026-03-17 19:27:55 +0000

    relationships

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,35 @@
 const User = require('./User');
-// we will add all models
+const Category = require('./Category');
+const Product = require('./Product');
+const Review = require('./Review');
+const Order = require('./Order');
+const OrderItem = require('./OrderItem');
+
+
+Category.hasMany(Product, { foreignKey: 'categoryId' });
+Product.belongsTo(Category, { foreignKey: 'categoryId' });
+
+Product.hasMany(Review, { foreignKey: 'productId' });
+Review.belongsTo(Product, { foreignKey: 'productId' });
+
+User.hasMany(Review, { foreignKey: 'userId' });
+Review.belongsTo(User, { foreignKey: 'userId' });
+
+User.hasMany(Order, { foreignKey: 'userId' });
+Order.belongsTo(User, { foreignKey: 'userId' });
+
+Order.hasMany(OrderItem, { foreignKey: 'orderId' });
+OrderItem.belongsTo(Order, { foreignKey: 'orderId' });
+
+Product.hasMany(OrderItem, { foreignKey: 'productId' });
+OrderItem.belongsTo(Product, { foreignKey: 'productId' });
+
 
 module.exports = {
   User,
+  Category,
+  Product,
+  Review,
+  Order,
+  OrderItem,
 };
